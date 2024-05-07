cfdca2baeb14b5ffd0c82fc6c02dfcd2d60218c4
ponomarev-vitaly <vp211082@gmail.com>
2024-05-07 03:30:10 -0500

    the code for web-site automation added, the test passed successfully

--- src/main/java/AutomatingSeleniumWebsiteKRNInformationMatrixEclipse.java
+++ src/main/java/AutomatingSeleniumWebsiteKRNInformationMatrixEclipse.java
@@ -1,4 +1,24 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AutomatingSeleniumWebsiteKRNInformationMatrixEclipse {
+public class AutomatingSeleniumWebsiteKRNInformationMatrixEclipse{
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chromedriver", "\"C:\\Users\\vvp\\Desktop\\SeleniumJarFiles\\chromedriver.exe\"");
+	    	
+	    WebDriver driver = new ChromeDriver();
+	
+	    driver.manage().window().maximize();
+	    
+	    driver.get("https://krninformatix.com/selenium/testing.html");
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[11]/div[1]/p[1]/button[1]")).click();
+	    Thread.sleep(3000);
+	    String altText = driver.switchTo().alert().getText();
+	    System.out.println(altText);
+	    driver.switchTo().alert().accept();
+	    driver.quit();
+	}
 
 }
