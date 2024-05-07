a9607a593b889879c12a91da51e9ae7b64e4bb9c
ponomarev-vitaly <vp211082@gmail.com>
2024-05-07 18:38:20 -0500

    added code to find a specific date in the calendar, to localize Account, Email/SMS, enter a phone number in the Mobile Phone area

--- src/main/java/AutomatingSeleniumWebsiteRedBus.java
+++ src/main/java/AutomatingSeleniumWebsiteRedBus.java
@@ -2,24 +2,43 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class AutomatingSeleniumWebsiteRedBus {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.redbus.in/");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//span[@class='dateText']")).click();
+		
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 seconds of explicit wait
+        driver.manage().window().maximize();
+        driver.get("https://www.redbus.in/");
+        
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        // Open the calendar
+        driver.findElement(By.xpath("//span[@class='dateText']")).click();
+
+        // Wait and click on the specific date
+        driver.findElement(By.xpath("//span[normalize-space()='21']")).click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        
+        // Wait and click on the Account in the right-top area        
+		driver.findElement(By.xpath("//span[normalize-space()='Account']")).click();
 		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("/html[1]/body[1]/section[1]/header[1]/div[1]/ul[2]/li[2]/div[1]/span[1]")).click();
+		// Wait and click on the Email/SMS in the list in the right-top area
+		driver.findElement(By.xpath("//span[normalize-space()='Email/SMS']")).click();
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("/html[1]/body[1]/section[1]/header[1]/div[1]/ul[2]/li[2]/div[2]/ul[1]/li[4]/span[1]")).click();
+		
+		// Wait and write a telephone number in the Mobile Number area
+		WebDriver driver1 = new ChromeDriver();
+		driver1.get("https://www.redbus.in/Home/SmsAndEmailTicket");
+		driver1.findElement(By.xpath("//input[@id='searchTicketMobileno']")).sendKeys("12345678");
 		Thread.sleep(3000);
-		driver.findElement(By.name("mobileno")).sendKeys("12345678");
+		
 		driver.quit();
+		
 
 	}
 
