db7b6927c824fa27bd5fc7d67b2200941bcb2076
ponomarev-vitaly <vp211082@gmail.com>
2024-05-08 05:06:22 -0500

    added a script to calculate all the links in the website, the text of every link printed out into the console

--- src/main/java/AutomatingSeleniumWebsiteKRNLinks.java
+++ src/main/java/AutomatingSeleniumWebsiteKRNLinks.java
@@ -1,9 +1,27 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutomatingSeleniumWebsiteKRNLinks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://krninformatix.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		List<WebElement> alllinks = driver.findElements(By.xpath("//a"));
+		int si = alllinks.size();
+		System.out.println(si);
+		
+		for(int i=0; i<si; i++) {
+			WebElement link = alllinks.get(i);
+			String textLink = link.getText();
+			System.out.println(textLink);
+		}
+		
 	}
-
 }
