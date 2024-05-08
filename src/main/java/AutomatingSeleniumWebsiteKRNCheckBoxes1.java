c17a0f1361b053b13ab8d18adfec1c02cf33e914
ponomarev-vitaly <vp211082@gmail.com>
2024-05-08 04:30:55 -0500

    automation for the website Check Box added: checked seleted/NOT, selected, again checked, unselected, selected&checked again, test is OK

--- src/main/java/AutomatingSeleniumWebsiteKRNCheckBoxes1.java
+++ src/main/java/AutomatingSeleniumWebsiteKRNCheckBoxes1.java
@@ -1,8 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutomatingSeleniumWebsiteKRNCheckBoxes1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://krninformatix.com/sample.html");
+		driver.manage().window().maximize();
+		
+		boolean checkBoxStatus = driver.findElement(By.id("rep")).isSelected();
+		System.out.println(checkBoxStatus);
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("rep")).click();
+		boolean checkBoxStatus1 = driver.findElement(By.id("rep")).isSelected();
+		System.out.println(checkBoxStatus1);
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("rep")).click();
+		boolean checkBoxStatus2 = driver.findElement(By.id("rep")).isSelected();
+		System.out.println(checkBoxStatus2);
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("rep")).click();
+		boolean checkBoxStatus3 = driver.findElement(By.id("rep")).isSelected();
+		System.out.println(checkBoxStatus3);
+		Thread.sleep(3000);
+		
+		driver.quit();
 
 	}
 
