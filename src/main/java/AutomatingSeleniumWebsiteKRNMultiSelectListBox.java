5c56baa05896140993aaf12723e9e45563d178b6
ponomarev-vitaly <vp211082@gmail.com>
2024-05-09 05:21:41 -0500

    added an automation code to select 3 cities Bangalore, Mumbai, Delhi, test is OK

--- src/main/java/AutomatingSeleniumWebsiteKRNMultiSelectListBox.java
+++ src/main/java/AutomatingSeleniumWebsiteKRNMultiSelectListBox.java
@@ -1,9 +1,34 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class AutomatingSeleniumWebsiteKRNMultiSelectListBox {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://krninformatix.com/sample.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		WebElement multiSelect = driver.findElement(By.id("cities"));
+		Select dd = new Select(multiSelect);
+		dd.selectByIndex(2);
+		Thread.sleep(3000);
+		dd.selectByValue("4");
+		Thread.sleep(3000);
+		dd.selectByVisibleText("Bangalore");
+		List<WebElement> allOptions = dd.getAllSelectedOptions();
+		int si = allOptions.size();
+		for(int i=0; i<si; i++) {
+			WebElement option1 = allOptions.get(i);
+			String tel1 = option1.getText();
+			System.out.println(tel1);
+			
+		}
 	}
 
 }
--- src/main/java/ExcelLibrary.java
+++ src/main/java/ExcelLibrary.java
@@ -1,4 +1,30 @@
+import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+import java.io.IOException;
+//import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.*;
+import org.openxml4j.exceptions.InvalidFormatException;
 
 public class ExcelLibrary {
-
+    public String getExcelData(String filePath, String sheetName, int rowNum, int cellNum) throws IOException, InvalidFormatException {
+        try (FileInputStream fis = new FileInputStream("C:\\Users\\vvp\\Desktop\\ExcelLibrary.xlsx");
+             Workbook wb = WorkbookFactory.create(fis)) {
+            Sheet sheet = wb.getSheet(sheetName);
+            Row row = sheet.getRow(rowNum);
+            Cell cell = row.getCell(cellNum);
+            return cell.getStringCellValue();
+        }
+    }
+    
+    public int getLastRowNumber(String filePath, String sheetName) throws IOException, InvalidFormatException {
+        try (FileInputStream fis = new FileInputStream("C:\\Users\\vvp\\Desktop\\ExcelLibrary.xlsx");
+             Workbook wb = WorkbookFactory.create(fis)) {
+            Sheet sheet = wb.getSheet(sheetName);
+            return sheet.getLastRowNum();
+        }
+    }
+    
+    public static void main(String[] args) {
+        // Your testing code here
+    }
 }
