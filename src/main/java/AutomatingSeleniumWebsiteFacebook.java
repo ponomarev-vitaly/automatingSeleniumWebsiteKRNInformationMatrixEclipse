6b72f5c66fa59dc6d0d3575eb321662151f342c4
ponomarev-vitaly <vp211082@gmail.com>
2024-05-09 06:49:26 -0500

    code for login automation added, fields e-mail, passwords localized, the royal_login_button clicked, test OK

--- src/main/java/AutomatingSeleniumWebsiteFacebook.java
+++ src/main/java/AutomatingSeleniumWebsiteFacebook.java
@@ -1,8 +1,29 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutomatingSeleniumWebsiteFacebook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		try {
+			driver.findElement(By.id("email")).sendKeys("v6676130@gmail.com");
+			driver.findElement(By.id("pass")).sendKeys("12345678");
+			Thread.sleep(5000);
+			driver.findElement(By.name("login")).click();
+			Thread.sleep(5000);
+			
+		}catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        driver.quit();
+	    }
+		
 
 	}
 
